99bea0bd2e44d93f982359f394ae08a00c0742f7
chiji1108 <shingo.pad@gmail.com>
2025-04-04 16:44:01 +0900

    Initial Commit

---
+++ MedicalShift/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  MedicalShift
+//
+//  Created by 千々岩真吾 on 2025/04/04.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ MedicalShift/MedicalShiftApp.swift
@@ -0,0 +1,17 @@
+//
+//  MedicalShiftApp.swift
+//  MedicalShift
+//
+//  Created by 千々岩真吾 on 2025/04/04.
+//
+
+import SwiftUI
+
+@main
+struct MedicalShiftApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
